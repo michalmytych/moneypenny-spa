53ea430a7c3fd6bb9c5a04f62523d73f20b103c0
Michal Mytych <michal.mytych@ftggroup.com>
2023-10-05 23:58:36 +0200

    Add flowbite react yarn package

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
+import TransactionsList from '../src/pages/transactions/TransactionsList';
 import './App.css';
 
 function App() {
   return (
     <div>
-      Hello!
+      <TransactionsList />
     </div>
   );
 }
